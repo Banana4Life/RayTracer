fc1d20a132733c72163e106db6bf75141f82a0d9
Phillip Schichtel <phillip.git@schich.tel>
2014-10-24 00:07:39 +0200

    use scala 2.11 and add scala-swing

--- build.sbt
+++ build.sbt
@@ -1,4 +1,10 @@
 name := "RayTracer"
 
 version := "1.0"
+
+scalaVersion := "2.11.1"
+
+libraryDependencies := Seq(
+  "org.scala-lang" % "scala-swing" % "2.11.0-M7"
+)
     
--- src/main/scala/RayTracer.scala
+++ src/main/scala/RayTracer.scala
@@ -1,5 +1,57 @@
-object RayTracer {
-  def main (args: Array[String]) {
-    println("Test")
+import java.awt.{Color, Rectangle, Dimension}
+import java.util.concurrent.{TimeUnit, Executors}
+import javax.swing.{WindowConstants, JFrame}
+
+import scala.swing.Swing._
+import scala.swing.event.{MousePressed, MouseEntered, ButtonClicked}
+import scala.swing._
+
+object RayTracer extends SimpleSwingApplication {
+
+  implicit def whateverToRunnable[F](f: => F) = new Runnable() { def run() { f } }
+
+  val executorService = Executors.newSingleThreadScheduledExecutor()
+
+  val top = new MainFrame() {
+    title = "Test Frame!"
+
+    preferredSize = 640 -> 480
+
+    resizable = false
+
+    reactions += {
+      case e => println(e.getClass.getName)
+    }
+
+    override def dispose(): Unit = {
+      super.dispose()
+
+      executorService.shutdownNow()
+    }
+
+    val panel = new Panel {
+
+      var i = 0
+      var s = -1
+
+      override def paint(g: Graphics2D): Unit = {
+        super.paint(g)
+
+        g.setColor(Color.RED)
+        g.drawString("lulzz", 100 + i, 100 + i * s)
+        i += 1
+        s *= -1
+      }
+    }
+
+    contents = panel
+
+    executorService.scheduleAtFixedRate({
+      panel.repaint()
+    }, 33, 33, TimeUnit.MILLISECONDS)
+
+    peer.setLocationRelativeTo(null)
+    peer.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
   }
+
 }
